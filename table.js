c8cad0a9cb357698176a924019a5bf70629c1218
Alex Lushiku <anxlus@tuta.io>
2021-01-10 20:40:27 +0100

    fix sort

--- table.js
+++ table.js
@@ -4,21 +4,25 @@ const sortByColumn = (idx) => {
 
   // FIXME: Change 4 to table height
   // TODO: Add toLowerCase()
-  for (let i = 1; i < 4; i++) {
+  for (let i = 1; i < table.getElementsByTagName("table")[0].rows.length - 2; i++) {
     let row = table.getElementsByTagName("tr")[i]
     rows.push(row)
   }
 
   // FIXME: Numbers don't work because they're a string
   for (let i = 0; i < rows.length - 1; i++) {
-    for (let j = 0; j < rows.length - 1; j++) {
+    for (let j = 0; j < rows.length - i - 1; j++) {
       var val = rows[j].getElementsByTagName("td")[idx].innerText
       var nextVal = rows[j+1].getElementsByTagName("td")[idx].innerText
 
+
+      // Convert strings to numbers
       if (!isNaN(parseInt(val))) {
         val = parseInt(val);
         nextVal = parseInt(nextVal);
       }
+      
+      // Swap values
       if (val > nextVal) {
         let temp = rows[j]
         rows[j] = rows[j+1]
