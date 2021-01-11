8e4bd9dda89015cb228af77817b6593542c89b72
andrulonis <andrulonis@spoko.pl>
2021-01-11 16:29:11 +0100

    Update sorter.js

--- js/sorter.js
+++ js/sorter.js
@@ -1,7 +1,4 @@
 $(document).ready(() => {
-  let len = document.getElementById("table-featured").rows.length;
-  console.log(len)
-
   $("#table-featured").tablesorter({
     sortInitialOrder: "asc",
     headers: {
