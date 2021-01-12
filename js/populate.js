79b4a2c6c8a83d569cf778cc1907eecc22ba1996
andrulonis <andrulonis@spoko.pl>
2021-01-12 12:56:12 +0100

    sa

--- js/populate.js
+++ js/populate.js
@@ -1,7 +1,14 @@
 function populate() {
   $.get("https://wt.ops.labs.vu.nl/api21/e502cf1e", (data) => {
+
     let $tbody = $("#table-featured > tbody");
-    
+    let rowsNum = $("#table-featured tr").length;
+    console.log(rowsNum)
+
+    for (let i = rowsNum - 2; i > 0; i--) {
+      document.getElementById("table-featured").deleteRow(i);
+    }
+
     for (let i = 0; i < data.length; i++) {
       let row = `
         <tr>
