707ef181fef58deba80bf39730ef122cb9725f87
andrulonis <andrulonis@spoko.pl>
2021-01-12 13:21:27 +0100

    Update populate.js

--- js/populate.js
+++ js/populate.js
@@ -1,28 +1,34 @@
 function populate() {
-  $.get("https://wt.ops.labs.vu.nl/api21/e502cf1e", (data) => {
+  $.ajax({
+    url: "https://wt.ops.labs.vu.nl/api21/e502cf1e",
+    type: "GET",
+    success: (data) => {
+      let $tbody = $("#table-featured > tbody");
+      let rowsNum = $("#table-featured tr").length;
+      let table = document.getElementById("table-featured");
 
-    let $tbody = $("#table-featured > tbody");
-    let rowsNum = $("#table-featured tr").length;
-    let table = document.getElementById("table-featured");
+      for (let i = rowsNum - 2; i > 0; i--) {
+        table.deleteRow(i);
+      }
 
-    for (let i = rowsNum - 2; i > 0; i--) {
-      table.deleteRow(i);
+      for (let i = 0; i < data.length; i++) {
+        let row = `
+          <tr>
+            <td>${data[i].product}</td>
+            <td>${data[i].origin}</td>
+            <td>${data[i].best_before_date}</td>
+            <td>${data[i].amount}</td>
+            <td><img src="${data[i].image}" alt="${data[i].product}"></td>
+          </tr>`;
+        
+        $tbody.prepend(row);
+      }
+      table.parentNode.reset();
+      $($tbody).trigger("update")
+    },
+    error: (data) => {
+      console.log(data);
     }
-
-    for (let i = 0; i < data.length; i++) {
-      let row = `
-        <tr>
-          <td>${data[i].product}</td>
-          <td>${data[i].origin}</td>
-          <td>${data[i].best_before_date}</td>
-          <td>${data[i].amount}</td>
-          <td><img src="${data[i].image}" alt="${data[i].product}"></td>
-        </tr>`;
-      
-      $tbody.prepend(row);
-    }
-    table.parentNode.reset();
-    $($tbody).trigger("update")
   })
 }
 
