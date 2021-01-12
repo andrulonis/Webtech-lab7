4e61e2d5c9f827eb1d40a8fd5ca5e7443cafd771
andrulonis <andrulonis@spoko.pl>
2021-01-12 10:18:58 +0100

    Update reset.js

--- js/reset.js
+++ js/reset.js
@@ -14,7 +14,7 @@ const resetTable = () => {
       $(".btn-reset").html("There was an error contacting the server: " + jqXHR.status + " " + jqXHR.responseText);
     }
     /*$.ajax({
-      url: "https://api.jsonbin.io/b/5ffc4e878aa7af359da8a015",
+      url: "https://wt.ops.labs.vu.nl/api21/e502cf1e",
       type: "GET",
       success: (res) => alert(res),
       error: (xhr, status, err) => alert(err)
