a74a1c5bd6812b2d57a14e7db2df1e32cbc13722
Alex Lushiku <anxlus@tuta.io>
2021-01-14 14:03:22 +0100

    indentation

--- scripts.js
+++ scripts.js
@@ -35,13 +35,13 @@ function populate() {
 
       for (let i = 0; i < data.length; i++) {
         let row = `
-        <tr>
-            <td>${data[i].product}</td>
-            <td>${data[i].origin}</td>
-            <td>${data[i].best_before_date}</td>
-            <td>${data[i].amount}</td>
-            <td><img src="${data[i].image}" alt="${data[i].product}"></td>
-        </tr>`;
+          <tr>
+              <td>${data[i].product}</td>
+              <td>${data[i].origin}</td>
+              <td>${data[i].best_before_date}</td>
+              <td>${data[i].amount}</td>
+              <td><img src="${data[i].image}" alt="${data[i].product}"></td>
+          </tr>`;
         
         $tbody.prepend(row);
       };
