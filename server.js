ffc4047895fb30cfb26f8157e469f24c71e31213
Alex Lushiku <anxlus@tuta.io>
2021-01-19 10:13:54 +0100

    clean server.js

--- server.js
+++ server.js
@@ -1,79 +1,60 @@
 // https://www.sqlitetutorial.net/sqlite-nodejs/
-// Database setup:
-// First: Our code will open a sqlite database file for you, and create one if it not exists already.
-// We are going to use the variable "db' to communicate to the database:
-// If you want to start with a clean sheet, delete the file 'products.db'.
-// It will be automatically re-created and filled with one example item.
 
 const sqlite = require('sqlite3').verbose();
-let db = my_database('./products.db');
+const express = require("express");
 
-var express = require("express");
-var app = express();
+const db = createDatabase('./products.db');
+const app = express();
 
-// middleware
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-
-// ###############################################################################
-// Routes
-//
 // TODO: Add your routes here and remove the example routes once you know how
-//       everything works.
-// ###############################################################################
 
-// This example route responds to http://localhost:3000/hello with an example JSON object.
-// Please test if this works on your own device before you make any changes.
-
-app.get("/hello", function(req, res) {
-    response_body = {'Hello': 'World'} ;
-
-    // This example returns valid JSON in the response, but does not yet set the
-    // associated HTTP response header.  This you should do yourself in your
-    // own routes!
-    res.json(response_body) ;
-});
-
-// This route responds to http://localhost:3000/db-example by selecting some data from the
-// database and return it as JSON object.
-// Please test if this works on your own device before you make any changes.
-app.get('/db-example', function(req, res) {
-    // Example SQL statement to select the name of all products from a specific brand
+/*app.get('/db-example', function(req, res) {
     db.all(`SELECT * FROM products WHERE product=?`, ['Apples'], function(err, rows) {
 
     	// TODO: add code that checks for errors so you know what went wrong if anything went wrong
     	// TODO: set the appropriate HTTP response headers and HTTP response codes here.
 
-    	// # Return db response as JSON
     	return res.json(rows)
     });
+});*/
+
+app.get("/products", (req, res) => {
+
 });
 
-app.post('/post-example', function(req, res) {
-	// This is just to check if there is any data posted in the body of the HTTP request:
+app.post("/products", (req, res) => {
 	console.log(req.body);
 	return res.json(req.body);
 });
 
+app.put("/products", (req, res) => {
 
-// ###############################################################################
-// This should start the server, after the routes have been defined, at port 3000:
+});
 
-app.listen(3000);
-console.log("Your Web server should be up and running, waiting for requests to come in. Try http://localhost:3000/hello");
+app.delete("/products", (req, res) => {
+  
+});
 
-// ###############################################################################
-// Some helper functions called above
-function my_database(filename) {
-	// Conncect to db by opening filename, create filename if it does not exist:
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:3000/${PORT}...`);
+});
+
+// === DATABASE === //
+
+function createDatabase(filename) {
 	var db = new sqlite.Database(filename, (err) => {
-  		if (err) {
-			console.error(err.message);
-  		}
-  		console.log('Connected to the products database.');
-	});
-	// Create our products table if it does not exist already:
+    if (err) {
+      console.error(err.message);
+    }
+
+    console.log('Connected to the products database.');
+  });
+  
 	db.serialize(() => {
 		db.run(`
         	CREATE TABLE IF NOT EXISTS products
@@ -83,8 +64,9 @@ function my_database(filename) {
         	best_before_date 	CHAR(20) NOT NULL,
           amount  CHAR(20) NOT NULL,
         	image   CHAR(254) NOT NULL
-        	)`);
-		db.all(`select count(*) as count from products`, function(err, result) {
+          )`);
+          
+		db.all(`select count(*) as count from products`, (err, result) => {
 			if (result[0].count == 0) {
 				db.run(`INSERT INTO products (product, origin, best_before_date, amount, image) VALUES (?, ?, ?, ?, ?)`,
 				["Apples", "The Netherlands", "November 2019", "100kg", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Apples.jpg/512px-Apples.jpg"]);
@@ -93,6 +75,7 @@ function my_database(filename) {
 				console.log("Database already contains", result[0].count, " item(s) at startup.");
 			}
 		});
-	});
+  });
+  
 	return db;
 }
